9d8b547451d40468bf6e3b77e32ce3759e631817
wladm <wladmyr@visiondevelopment.co>
2020-09-28 16:31:04 -0300

    Liskov Substitution Principle

---
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -0,0 +1,18 @@
+import Appointment from '../entities/Appointment';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
+
+import { EntityRepository, Repository } from 'typeorm';
+
+@EntityRepository(Appointment)
+class AppointmentsRepository extends Repository<Appointment> implements IAppointmentsRepository {
+    public async findByDate(date: Date): Promise<Appointment | undefined> {
+        const findAppointment = await this.findOne({
+            where: { date },
+        });
+
+        return findAppointment;
+    }
+}
+
+export default AppointmentsRepository;
