458156e1770e96900677ff6cfa49f97ddd68517e
wladm <wladmyr@visiondevelopment.co>
2020-09-28 22:31:46 -0300

    Teste de agendamentos com coverage report

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -2,6 +2,8 @@ import 'reflect-metadata';
 import CreateAppointmentService from './CreateAppointmentService';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 
+import AppError from '@shared/errors/AppError';
+
 describe('CreateAppointment', () => {
     //Ler semanticamente melhor do que test
     it('should be able to create a new appointment', async () => {
@@ -16,10 +18,27 @@ describe('CreateAppointment', () => {
         });
 
         expect(appointment).toHaveProperty('uuid');
-        expect(appointment.provider_id).toBe('1234567')
+        expect(appointment.provider_id).toBe('1234567');
     });
 
-    // it('should not be able to create two appointments on the same time', () => {
-    //     expect().toBe();
-    // })
+    it('should not be able to create two appointments on the same time', async () => {
+        const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentsRepository,
+        );
+
+        const appointmentDate = new Date(2020, 4, 9, 11);
+
+        await createAppointment.execute({
+            date: appointmentDate,
+            provider_id: '1234567',
+        });
+
+        expect(
+            createAppointment.execute({
+                date: appointmentDate,
+                provider_id: '1234567',
+            }),
+        ).rejects.toBeInstanceOf(AppError);
+    });
 });
