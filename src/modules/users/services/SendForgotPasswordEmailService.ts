7f640ca30457f95210e3701da5cabe001fd28c25
Wladmyr <wladmyralmeida@gmail.com>
2020-10-02 09:40:46 -0300

    Template Engine

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IUsersRepository from '../repositories/IUsersRepository';
@@ -32,6 +33,13 @@ export default class SendForgotPasswordEmailService {
 
         const { token } = await this.userTokensRepository.generate(user.id);
 
+        const forgotPasswordTemplate = path.resolve(
+            __dirname,
+            '..',
+            'views',
+            'forgot_password.hbs',
+        );
+
         await this.mailProvider.sendMail({
             to: {
                 name: user.name,
@@ -39,12 +47,12 @@ export default class SendForgotPasswordEmailService {
             },
             subject: 'TeAgendei - Recuperação de senha',
             templateData: {
-                template: 'Olá, {{name}}: {{token}}',
+                file: forgotPasswordTemplate,
                 variables: {
                     name: user.name,
-                    token,
-                }
-            }
+                    link: `http://localhost:3000/reset_password?token=${token}`,
+                },
+            },
         });
     }
 }
