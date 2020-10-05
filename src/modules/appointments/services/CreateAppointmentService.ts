75c2e6b1b4fef86aa3328f55e4eaec5a3aa627a3
Wladmyr <wladmyralmeida@gmail.com>
2020-10-05 13:44:26 -0300

    Regras do agendamento not 100%

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,6 +1,6 @@
 import { injectable, inject } from 'tsyringe';
 
-import { startOfHour } from 'date-fns';
+import { startOfHour, isBefore, getHours } from 'date-fns';
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
@@ -25,6 +25,20 @@ class CreateAppointmentService {
         user_id,
     }: IRequest): Promise<Appointment> {
         const appointmentDate = startOfHour(date);
+
+        //Data manualmente colocada do teste,
+        if (isBefore(appointmentDate, Date.now())) {
+            throw new AppError("You can't create an appointment on past date");
+        }
+
+        if (user_id === provider_id) {
+            throw new AppError("You can't create an appointment with yourself");
+        }
+
+
+        if (getHours(appointmentDate) < 8 || getHours(appointmentDate) > 17) {
+            throw new AppError("You can't only create appointment between 8 am and 5pm");
+        }
 
         const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
             appointmentDate,
