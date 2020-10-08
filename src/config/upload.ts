fcea88e693ae30351b49ae062ee02b32abd6a185
Wladmyr <wladmyralmeida@gmail.com>
2020-10-07 23:10:49 -0300

    Organizando Container e fazendo upload de arquivos para o S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,40 @@
-import multer from "multer";
-import path from "path";
-import crypto from "crypto";
+import path from 'path';
+import crypto from 'crypto';
+import multer, { StorageEngine } from 'multer';
 
-const tmpFolder = path.resolve(__dirname, "..", "..", "tmp");
-
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: { storage: StorageEngine };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const filename = `${fileHash}-${file.originalname}`;
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString("hex");
-      const fileName = `${fileHash}-${file.originalname}`;
-
-      return callback(null, fileName);
+        return callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-2',
     },
-  }),
-};
+  },
+} as IUploadConfig;
